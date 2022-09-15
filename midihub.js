6c0b060afe595f060bfae5ab3e7e16edcde3698a
mausi <manster161@gmail.com>
2022-09-15 14:54:20 +0200

    Initial comit

---
+++ midihub.js
@@ -0,0 +1,12 @@
+const mapping = new Map();
+var midihub = {
+    addMapping: function (ccvalue, callback){
+        mapping.set(ccvalue, callback);
+    },
+    newMessage: function (message){
+        if (mapping.has(message[1])){
+            mapping.get(message[1])(message[2]);
+        };
+    }
+};
+module.exports = midihub;
---
+++ mormalmidi.js
@@ -0,0 +1,45 @@
+const midi = require('midi');
+const fs = require('fs');
+const { Console } = require('node:console');
+const config = require('config');
+const hub = require('./midihub.js');
+const portNum = config.get('midi.port');
+
+const mormalmidi = {
+     init : function (){
+        console.log(`midi port from config ${config.midi.port}`);
+
+        var portName = "";
+        const input = new midi.Input(); 
+        
+        var count = input.getPortCount();
+        
+        const output = fs.createWriteStream(config.get('logging.std'));
+        const errorOutput = fs.createWriteStream(config.get('logging.err'));
+        // Custom simple logger
+        const logger = new Console({ stdout: output, stderr: errorOutput });
+        
+        for (var i = 0; i < count; i++){
+           logger.log(input.getPortName(i));
+        }
+        
+        input.on('message', (_, message) => {
+            hub.newMessage(message);
+        })
+        
+        portName = input.getPortName(portNum);
+        logger.log(`Opening port: ${portName}`)
+        console.log(`Opening port: ${portName}`)
+        input.openPort(0);
+    },
+
+    addMapping : function (cc, callback){
+        hub.addMapping(cc, callback);
+    },
+
+    close : function(){
+        input.closePort();
+    }
+}
+
+module.exports = mormalmidi;
